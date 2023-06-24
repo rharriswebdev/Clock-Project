b4d3ec3549c3eccc6df4ef6d50baf1b8f2f1ecec
rharriswebdev <rharr55@gmail.com>
2023-06-23 23:17:27 -0500

    updated project

--- app.js
+++ app.js
@@ -20,3 +20,29 @@ function setRotation(element, rotationRatio) {
 }
 
 setClock()
+
+setInterval(showTime, 1000);
+function showTime() {
+    let time = new Date();
+    let hour = time.getHours();
+    let min = time.getMinutes();
+    am_pm = "AM";
+ 
+    if (hour > 12) {
+        hour -= 12;
+        am_pm = "PM";
+    }
+    if (hour == 0) {
+        hr = 12;
+        am_pm = "AM";
+    }
+ 
+    hour = hour < 10 ? "0" + hour : hour;
+    min = min < 10 ? "0" + min : min;
+   
+ 
+    let currentTime = hour + ":" + min + " " + "<span>" + am_pm + "</span";
+ 
+    document.getElementById("digital").innerHTML = currentTime;
+}
+showTime();
